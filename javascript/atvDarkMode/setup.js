ea8b8aa1aae7afdcfb0a84d1b2d23fcf74894304
matheus corregiari <matheus_corregiari@hotmail.com>
2021-11-28 22:26:10 -0300

    Capturando elementos HTML e alterando o padrão através do js

---
+++ javascript/atvDarkMode/setup.js
@@ -0,0 +1,34 @@
+function changeMode(){
+    changeClasses();
+    changeText();
+}
+
+function changeClasses(){
+    button.classList.toggle(darkModeClass);
+    title.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+}
+
+function changeText(){
+   const lightMode = "Light Mode";
+   const darkMode = "Dark Mode";
+   
+    if(body.classList.contains(darkModeClass)){
+        button.innerHTML = lightMode;
+        title.innerHTML = darkMode + " ON";
+        return;
+    }
+
+    button.innerHTML = darkMode;
+    title.innerHTML = lightMode + " ON";
+
+}
+
+
+const darkModeClass = 'dark-mode';
+const button = document.getElementById('modeSelector');
+const title = document.getElementById('pageTitle');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+button.addEventListener('click', changeMode)
